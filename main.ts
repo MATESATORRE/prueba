2fb9cc60f9a75ce9081faeb5941febfe70c972fb
MATESATORRE <127900201+MATESATORRE@users.noreply.github.com>
2023-04-01 08:59:49 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,18 +11,18 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 input.onGesture(Gesture.Shake, function () {
-    basic.showString("ECUACIÓN: " + A + "X^2 + " + B + "X + " + C + " = 0")
+    basic.showString("ECUACION: " + A + "X^2 + " + B + "X + " + C + " = 0")
     if (B * B - 4 * (A * C) < 0) {
-        basic.showString("NO TIENE SOLUCIÓN" + "Mundo")
+        basic.showString("NO TIENE SOLUCION" + " Discriminante negativo")
     } else {
         basic.pause(1000)
-        basic.showString("PRIMERA SOLUCIÓN" + (0 - B + Math.sqrt(B * B - 4 * (A * C))) / (2 * A))
-        basic.showString("SEGUNDA SOLUCIÓN" + (0 - B - Math.sqrt(B * B - 4 * (A * C))) / (2 * A))
+        basic.showString("PRIMERA SOLUCION: " + (0 - B + Math.sqrt(B * B - 4 * (A * C))) / (2 * A))
+        basic.showString("SEGUNDA SOLUCION: " + (0 - B - Math.sqrt(B * B - 4 * (A * C))) / (2 * A))
     }
 })
 input.onButtonPressed(Button.AB, function () {
     C = randint(-20, 20)
-    basic.showString("El término independiente es  " + C)
+    basic.showString("El termino independiente es  " + C)
 })
 input.onButtonPressed(Button.B, function () {
     B = randint(-20, 20)
